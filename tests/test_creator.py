9c527a72f3c6111e7d28a604d39031f8503b6ff1
lakerc <colton.laker26@gmail.com>
2018-04-24 13:59:32 -0400

    Upload tests, fix random pages not showing creator, and fix creator info disappearing when favoriting a page.

---
+++ tests/test_creator.py
@@ -0,0 +1,23 @@
+from wiki import *
+import creator_module
+
+class PageHasCreatorTestCase(WikiBaseTestCase):
+
+    def test_page_has_creator(self):
+        """
+            Assert that given page has a json entry that isn't empty
+        """
+        data = creator_module.get_page_data('colton')
+        assert data
+        
+    def test_search_page(self):
+        """
+            Assert that pages matching creator's name exists
+        """
+        pages = self.index()
+        regex = re.compile('colton', re.IGNORECASE if ignore_case else 0)
+        matched = []
+        matched = creator_module.search_by_creator(pages, regex)
+        assert matched
+        
+        
